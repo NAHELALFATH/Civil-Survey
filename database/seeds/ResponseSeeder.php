807adaa9960aa9380d6d3402985b4550009aaf8b
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-03-04 15:41:18 +0700

    Seeder can now fill survey data.

--- database/seeds/ResponseSeeder.php
+++ database/seeds/ResponseSeeder.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Seeder;
 use App\Response;
 use App\PublicTransportUserResponse;
+use App\Enums\RespondentType;
+use App\SurveyData;
+use App\Type;
 
 class ResponseSeeder extends Seeder
 {
@@ -13,10 +16,21 @@ class ResponseSeeder extends Seeder
      */
     public function run()
     {
-        DB::transaction(function() {
-            factory(Response::class, 100)
+        $types = Type::query()
+            ->select('id', 'name', "respondent_type")
+            ->with([
+                'criteria:id,name,type_id',
+                'criteria.sub_criteria:id,name,criterion_id',
+                'criteria.sub_criteria.alternatives:id,name,sub_criterion_id',
+            ])
+            ->orderBy("id")
+            ->get()
+            ->groupBy("respondent_type");
+
+        DB::transaction(function() use($types) {
+            factory(Response::class, 50)
                 ->make()
-                ->each(function (Response $response) {
+                ->each(function (Response $response) use($types) {
 
                     $extra_data = factory(PublicTransportUserResponse::class)
                         ->states(array_random(PublicTransportUserResponse::STATES))
@@ -27,6 +41,23 @@ class ResponseSeeder extends Seeder
                         ->associate($extra_data);
 
                     $response->save();
+
+                    foreach ($types[(string) RespondentType::public_transport_user()] as $type) {
+                        foreach ($type->criteria as $criterion) {
+                            foreach ($criterion->sub_criteria as $sub_criterion) {
+                                foreach ($sub_criterion->alternatives as $alternative) {
+                                    SurveyData::create([
+                                        'response_id' => $response->id,
+                                        'type_id' => $type->id,
+                                        'criterion_id' => $criterion->id,
+                                        'sub_criterion_id' => $sub_criterion->id,
+                                        'alternative_id' => $alternative->id,
+                                        'rating' => rand(1, 5)
+                                    ]);
+                                }
+                            }
+                        }
+                    }
                 });
         });
     }
